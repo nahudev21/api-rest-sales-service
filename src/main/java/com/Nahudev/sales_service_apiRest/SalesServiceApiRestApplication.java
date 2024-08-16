08e9c0426ea52e6892ba897c3d3ebd3e7a63bd6d
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-08-16 20:06:19 -0300

    Configuracion de la DB y dependencias

--- src/main/java/com/Nahudev/sales_service_apiRest/SalesServiceApiRestApplication.java
+++ src/main/java/com/Nahudev/sales_service_apiRest/SalesServiceApiRestApplication.java
@@ -1,10 +1,17 @@
 package com.Nahudev.sales_service_apiRest;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SalesServiceApiRestApplication {
+
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesServiceApiRestApplication.class, args);
