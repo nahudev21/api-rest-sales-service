363c9e165bb4557fbd99d2fc6d1607297ae6f5f1
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-08-16 19:56:08 -0300

    Subi el proyecto base

---
+++ src/main/java/com/Nahudev/sales_service_apiRest/SalesServiceApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.Nahudev.sales_service_apiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesServiceApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesServiceApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Nahudev/sales_service_apiRest/SalesServiceApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nahudev.sales_service_apiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesServiceApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
