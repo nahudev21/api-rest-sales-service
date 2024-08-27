a9a43ce4917b414f9436551ed244a7a25f54eeda
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-08-27 20:22:46 -0300

    Registro del servicio en Eureka Server

--- src/main/java/com/Nahudev/sales_service_apiRest/SalesServiceApiRestApplication.java
+++ src/main/java/com/Nahudev/sales_service_apiRest/SalesServiceApiRestApplication.java
@@ -3,10 +3,12 @@ package com.Nahudev.sales_service_apiRest;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class SalesServiceApiRestApplication {
 
--- src/main/java/com/Nahudev/sales_service_apiRest/repository/IShoppingCartClientRepository.java
+++ src/main/java/com/Nahudev/sales_service_apiRest/repository/IShoppingCartClientRepository.java
@@ -2,13 +2,19 @@ package com.Nahudev.sales_service_apiRest.repository;
 
 import com.Nahudev.sales_service_apiRest.dto.ShoppingCartFromUserClientDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "shoppingCartClient", url = "http://localhost:8082/shoppingCart")
+@FeignClient(name = "shoppingCart-service-apiRest")
 public interface IShoppingCartClientRepository {
 
-    @GetMapping("/user")
+    public String URL = "/shoppingCart";
+
+    @GetMapping(URL + "/user")
     public ShoppingCartFromUserClientDTO getAllProductsFromShoppingCart(@RequestParam(value = "userId") Long userId);
 
+    @DeleteMapping(URL + "/deleteShoppingCart/{userId}")
+    public String deleteShoppingCart(@PathVariable(name = "userId") Long userId);
 }
